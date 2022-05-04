baf2a517c460e22fce7354efc958b5722958c705
anders246 <52299619+anders246@users.noreply.github.com>
2022-05-03 20:54:53 -0400

    updated api url

--- amazon-clone/src/axios.js
+++ amazon-clone/src/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/clone-5a579/us-central1/api/', // API url
+  // API url
+  baseURL: 'https://us-central1-clone-5a579.cloudfunctions.net/api',
+  // baseURL: 'http://localhost:5001/clone-5a579/us-central1/api/',
 });
 
 export default instance;
